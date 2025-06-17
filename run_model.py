222199e14769a0a8214fb2844a2e5af5f9921fea
meowmeowahr <meowmeowahr@gmail.com>
2025-06-16 21:04:46 -0500

    Update training script, test script

--- run_model.py
+++ run_model.py
@@ -1,8 +1,9 @@
+import time
 import cv2
 from ultralytics import YOLO
 
 # Load the model (replace with your own model if needed)
-model = YOLO('runs2/detect/train/weights/best.pt')  # or 'yolov8s.pt', custom_model.pt, etc.
+model = YOLO('runs/detect/train6/weights/best.pt')  # or 'yolov8s.pt', custom_model.pt, etc.
 
 # Optional: move to GPU for performance
 model.to('cuda:0')
@@ -14,19 +15,24 @@ if not cap.isOpened():
     raise RuntimeError("Failed to open camera.")
 
 # (Optional) Set resolution
-# cap.set(cv2.CAP_PROP_FRAME_WIDTH, 1280)
-# cap.set(cv2.CAP_PROP_FRAME_HEIGHT, 720)
+cap.set(cv2.CAP_PROP_FRAME_WIDTH, 1280)
+cap.set(cv2.CAP_PROP_FRAME_HEIGHT, 720)
 
 while True:
+    proc_start = time.monotonic()
     ret, frame = cap.read()
     if not ret:
         break
 
     # Run inference, set stream=True to avoid blocking
-    results = model.predict(frame, imgsz=640, conf=0.5, stream=False, verbose=True, device='cpu')
+    results = model.predict(frame, imgsz=640, conf=0.5, stream=False, verbose=False, device='cuda:0')
 
     # Visualize results
     annotated_frame = results[0].plot()
+
+    fps = 1 / (time.monotonic() - proc_start)
+    print(fps)
+    cv2.putText(annotated_frame, f"{round(fps, 2)}FPS", (50, 50), 3, 2, (0, 255, 0), 2)
 
     # Display
     cv2.imshow("YOLOv8 Live", annotated_frame)
